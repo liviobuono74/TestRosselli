4214e5da057f4ce74ebebfe31c7a372b9054bb7a
LivioBuono <livio.buono74@gmail.com>
2017-12-07 11:09:43 +0100

    Aggiunto progetto WPF

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Writeline("ciao ciao");
+            Console.WriteLine("ciao ciao");
         }
     }
 }
